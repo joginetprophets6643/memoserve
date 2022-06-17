c7f8746fd72765b05e08b2dd9cf2788063917a4c
jugendra <jugendra.singh@netprophetsglobal.com>
2022-06-17 10:56:53 +0530

    Post Routes Set

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,4 +1,5 @@
 const  express = require('express');
+
 const router = express.Router()
 const  auth = require('../middleware/auth.js')
 const postController =  require('../controllers/postController.js');
